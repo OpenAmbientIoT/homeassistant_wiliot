b10742e4cc005a359433fac82e2a8a01567912c6
elie <leferdman@gmail.com>
2023-02-16 00:05:49 -0500

    unnest

--- custom_components/__init__.py
+++
@@ -1,7 +0,0 @@
-"""An integration for provisioning Wiliot Pixels to Home Assistant."""
-
-from homeassistant improt core
-
-
-async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:
-  return True
