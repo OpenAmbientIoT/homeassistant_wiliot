c69e7be6d113ba6685df3865bf365fc2383a3f12
Elie <52466317+ezfman@users.noreply.github.com>
2023-02-16 00:48:39 -0500

    Update __init__.py
    Signed-off-by: Elie <52466317+ezfman@users.noreply.github.com>

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 """An integration for provisioning Wiliot Pixels to Home Assistant."""
 
-from homeassistant improt core
+from homeassistant import core
 
 
 async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:
