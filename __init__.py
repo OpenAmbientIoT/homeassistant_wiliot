cc6fe4121c9fe4bf16146667d5cf5aba43e2620b
elie <leferdman@gmail.com>
2023-02-16 21:06:39 -0500

    wipe away the sins of the past

--- __init__.py
+++
@@ -1,7 +0,0 @@
-"""An integration for provisioning Wiliot Pixels to Home Assistant."""
-
-from homeassistant import core
-
-
-async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:
-  return True
